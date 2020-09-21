c13b9be047cf6f01b817b8e4798bf7044261255b
Michelle Kristensen <mich5h03@edu.easj.dk>
2020-09-21 10:58:41 +0200

    Add project files.

---
+++ Obligatorisk1.1/Bike.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Obligatorisk1._1
+{
+    public class Bike
+    {
+        private int _id;
+        private string _color;
+        private int _price;
+        private int _gear;
+
+
+        public Bike(int id, string color, int price, int gear)
+        {
+            ID = id;
+            Color = color;
+            Price = price;
+            Gear = gear;
+        }
+
+
+        public int ID
+        {
+            get { return _id; }
+            set 
+            {
+                    _id = value;
+            }
+        }
+
+        public string Color
+        {
+            get { return _color; }
+            set 
+            {
+                if (value.Length < 1)
+                {
+                    throw new Exception("please insert a color");
+                }
+                else
+                {
+                    _color = value;
+                }
+            }
+        }
+
+        public int Price
+        {
+            get { return _price; }
+            set
+            {
+                if (value > 0)
+                {
+                    _price = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("please insert a price (price cannot be negative values");
+                }
+            }
+        }
+
+        public int Gear
+        {
+            get { return _gear; }
+            set 
+            {
+                if (value >= 3 && value <= 32)
+                {
+                    _gear = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("please insert numbers of gears (between 3 and 32)");
+                }
+            }
+        }
+
+
+    }
+}
---
+++ Obligatorisk1.1Tests/BikeTests.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Obligatorisk1._1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Obligatorisk1._1.Tests
+{
+    [TestClass()]
+    public class BikeTests
+    {
+
+        private Bike bikeOne;
+        private Bike bikeTwo;
+        private Bike bikeThree;
+        private Bike bikeFour;
+        private Bike bikeFive;
+
+        [TestInitialize]
+
+        public void Initialize()
+        {
+            bikeOne = new Bike(1, "red", 50, 20);
+            bikeTwo = new Bike(2, "blue", 70, 30);
+            bikeThree = new Bike(3, "Baby-pink", 150, 3);
+            bikeFour = new Bike(4, "Hot-pink", 200, 10);
+            bikeFive = new Bike(5, "Rainbow", 1000, 32);
+        }
+
+        [TestMethod()]
+        public void ColorTest()
+        {
+            Assert.AreEqual("red", bikeOne.Color);
+            Assert.AreEqual("Hot-pink", bikeFour.Color);
+            bikeTwo.Color = "Y";
+            Assert.AreEqual("Y", bikeTwo.Color);
+            Assert.ThrowsException<Exception>(() => bikeTwo.Color = "");
+            bikeTwo.Color = "A Very Vey Very Very Very Looooooooooooooooooong Color Name";
+            Assert.AreEqual("A Very Vey Very Very Very Looooooooooooooooooong Color Name", bikeTwo.Color);
+        }
+
+        [TestMethod()]
+        public void PriceTest()
+        {
+            Assert.AreEqual(1000, bikeFive.Price);
+            bikeFive.Price = 500000000;
+            Assert.AreEqual(500000000, bikeFive.Price);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => bikeFive.Price = -5000);
+        }
+
+        [TestMethod()]
+        public void GearTest()
+        {
+            Assert.AreEqual(20, bikeOne.Gear);
+            bikeFive.Gear = 32;
+            Assert.AreEqual(32, bikeFive.Gear);
+            bikeFive.Gear = 3;
+            Assert.AreEqual(3, bikeFive.Gear);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => bikeThree.Gear = 2);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => bikeThree.Gear = 33);
+        }
+    }
+}
